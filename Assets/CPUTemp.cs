0659d0eec1b441a3754d29a8f8f74b0c4fe680a8
Slivre <66972691+Slivre@users.noreply.github.com>
2024-05-10 22:04:58 -0400

    CPU Temp Prefab

---
+++ Assets/CPUTemp.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CPUTemp : MonoBehaviour
+{
+    public float CPUTemperture;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
