725811feac1d78b96ba3d74166d639fad6737c5f
Slivre <66972691+Slivre@users.noreply.github.com>
2024-05-11 17:13:01 -0400

    WOM Page Installed

---
+++ Assets/ButtonEffects.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonEffects : MonoBehaviour
+{
+    public float GrowSize;
+    Animator ac;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ac = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
