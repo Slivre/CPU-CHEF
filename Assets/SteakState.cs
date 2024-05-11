6c84370cf9bbe1cce034caedd6be7d4b14561314
JimmyDing00 <dingyanj@sheridancollege.ca>
2024-05-10 23:03:26 -0400

    SteakState

---
+++ Assets/SteakState.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SteakCooking : MonoBehaviour
+{
+    public SpriteRenderer steakRenderer; 
+    public CPUTemp cpuTemp; 
+
+    public float cookTime = 0; 
+    public float requiredTime = 10; 
+
+    private Color rawColor = Color.red; 
+    private Color cookedColor = Color.yellow; 
+    private Color overcookedColor = Color.black; 
+
+    void Update()
+    {
+        if (cpuTemp != null)
+        {
+            
+            float temperature = cpuTemp.GetCurrentTemperature(); 
+
+            
+            cookTime += Time.deltaTime * temperature / 100;
+
+            
+            float cookPercent = cookTime / requiredTime;
+            if (cookPercent < 1)
+            {
+                steakRenderer.color = Color.Lerp(rawColor, cookedColor, cookPercent);
+            }
+            else
+            {
+                steakRenderer.color = Color.Lerp(cookedColor, overcookedColor, cookPercent - 1);
+            }
+        }
+    }
+}
