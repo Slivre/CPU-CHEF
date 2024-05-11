65d1510e403e0d96493b1a08d088db680018d281
JimmyDing00 <dingyanj@sheridancollege.ca>
2024-05-10 22:36:28 -0400

    CPUTempChange

--- Assets/CPUTemp.cs
+++ Assets/CPUTemp.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class CPUTemp : MonoBehaviour
 {
-    public float CPUTemperture;
+    public float CPUTemperture = 50.0f;
     // Start is called before the first frame update
     void Start()
     {
         
+    }
+
+    public float GetCurrentTemperature()
+    {
+        return CPUTemperture;
     }
 
     // Update is called once per frame
